b8defdfd03b2cb2482ff0d7af595f53dce2bf9ef
TheNovi <jakub.novi.novacek@gmail.com>
2019-01-30 23:48:43 +0100

    + scpClass upgrade

--- bml/scpClass.js
+++ bml/scpClass.js
@@ -1,8 +1,55 @@
 (function() {
-	var p = document.getElementsByTagName('p');
-	for (var i = 0; i < p.length; i++) {
-		if (p[i].innerHTML.search("<strong>Object Class:</strong>") == 0){
- 			p[i].innerHTML += '<br><strong>Novi Class:</strong> Definitely not "'+p[i].innerHTML.replace("<strong>Object Class:</strong>", "").trim()+'"'
+	var data = {
+	"name":"NC",
+	"default":"undefined",
+	"class": {
+		"s":"Safe",
+		"e":"Euclid",
+		"k":"Keter",
+		"t":"Thaumiel",
+		"n":"Neutralized"
+	},
+	"db":[{
+		"n": "002",
+		"c": "s"
+	},
+	{
+		"n": "503",
+		"c": "k"
+	},
+	{
+		"n": "650",
+		"c": "k"
+	},
+	{
+		"n": "999",
+		"c": "e"
+	}
+]}; // JSON data
+
+/*
+,
+	{
+		"n": "",
+		"c": ""
+	}
+*/
+
+var p = document.getElementsByTagName('p');
+for (var i = 0; i < p.length; i++) {
+	if (p[i].innerHTML.search("<strong>Object Class:</strong>") == 0){
+		if (p[i].innerText.search(data["name"] + ":") > -1) break;
+		p[i].innerHTML += "<br><strong>" + data["name"] + ":</strong> " + getNC(document.getElementById("page-title").innerText.trim().toLowerCase().replace("scp-", ""));
+		break;
+	}
+}
+
+function getNC(number){
+	for (var i = 0; i < data["db"].length; i++) {
+		if (data["db"][i]["n"] == number){
+			return data["class"][data["db"][i]["c"]];
 		}
 	}
+	return data["default"];
+}
 })();
