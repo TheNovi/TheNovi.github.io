f562b2c0992c0140f59d8a9f2252b7fabd621086
TheNovi <jakub.novi.novacek@gmail.com>
2019-06-06 16:01:54 +0200

    js: basic automated age changer

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,18 @@
 function footer(){
-	y = new Date().getFullYear()
-	a = [ y, y-1, y + ' - since your dead', '1997', 'NaN', 'Null', true, false]
-	//console.log(a)
-	document.getElementById("footer").innerHTML = document.getElementById("footer").innerHTML.replace('{}', a[Math.floor(Math.random()*a.length)]);
+	y = new Date().getFullYear();
+	a = [ y, y-1, y + ' - since your dead', '1997', 'NaN', 'Null', true, false];
+	d = document.getElementById("footer");
+	d.innerHTML = d.innerHTML.replace('{}', a[Math.floor(Math.random()*a.length)]);
+}
+
+function age() {
+	y = new Date().getFullYear() - 1997;
+	if ((new Date().getMonth() - 5) < 0) y--;
+	d = document.getElementById("age");
+	d.innerHTML = d.innerHTML.replace('{}', "I am just an " + y + " years old programmer, gamer, geek and otaku from Czech Republic.");
 }
 
 (function() {
 	footer();
+	age();
 })();
