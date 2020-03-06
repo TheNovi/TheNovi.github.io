a0b2d1bc7b61382a94f507fe01956d076424f5e2
TheNovi <jakub.novi.novacek@gmail.com>
2020-03-07 00:22:32 +0100

    Jinja2 mostly done

--- backend/gen_jinja.py
+++ backend/gen_jinja.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
 	for t in filter(lambda x: 'inc/' not in x, env.list_templates()):
 		print(t)
 		name = t.replace('\\', '/').split('/')[-1]
-		path = os.path.join(os.path.join('..', 'nudes', t.replace(name, '')))
+		path = os.path.join(os.path.join('..', t.replace(name, '')))
 		if not os.path.exists(path):
 			os.makedirs(os.path.join(*path.replace('\\', '/').split('/')[:-1]))
 		with open(os.path.join(path, name), 'w') as f:
-			f.writelines(env.get_template(t).render(path=path.replace('..\\nudes\\', '')))
+			f.writelines(env.get_template(t).render(path=path.replace('..\\', '')))
