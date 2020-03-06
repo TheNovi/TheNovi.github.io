22cd6e6099a2c0b6134ed398c0da10f81ef0454e
TheNovi <jakub.novi.novacek@gmail.com>
2020-03-06 18:22:34 +0100

    gen_jinja.py Now creates paths automatically

--- backend/gen_jinja.py
+++ backend/gen_jinja.py
@@ -1,3 +1,5 @@
+import os
+
 import jinja2
 
 # https://jinja.palletsprojects.com/en/2.11.x/templates/
@@ -5,5 +7,9 @@ if __name__ == '__main__':
 	env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'), autoescape=True, trim_blocks=True)
 	for t in filter(lambda x: 'inc/' not in x, env.list_templates()):
 		print(t)
-		with open(f'../nudes/{t}', 'w') as f:
+		name = t.replace('\\', '/').split('/')[-1]
+		path = os.path.join(os.path.join('..', 'nudes', t.replace(name, '')))
+		if not os.path.exists(path):
+			os.makedirs(os.path.join(*path.replace('\\', '/').split('/')[:-1]))
+		with open(os.path.join(path, name), 'w') as f:
 			f.writelines(env.get_template(t).render(body='Hi'))
